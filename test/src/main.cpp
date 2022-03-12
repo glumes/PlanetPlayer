5c9030be2c219a199d93f8bfc87cf9dfff23c63a
glumes <zhaoying9402@gmail.com>
2022-03-12 16:27:31 +0800

    播放器库工程运行 和 单测搭建 以及 Demo 工程搭建

---
+++ test/src/main.cpp
@@ -0,0 +1,30 @@
+/**
+ * ,------. ,--.                        ,--.      ,------. ,--.
+ * |  .--. '|  | ,--,--.,--,--, ,---. ,-'  '-.    |  .--. '|  | ,--,--.,--. ,--.,---. ,--.--.
+ * |  '--' ||  |' ,-.  ||      \ .-. :'-.  .-'    |  '--' ||  |' ,-.  | \  '  /| .-. :|  .--'
+ * |  | --' |  |\ '-'  ||  ||  \   --.  |  |      |  | --' |  |\ '-'  |  \   ' \   --.|  |
+ * `--'     `--' `--`--'`--''--'`----'  `--'      `--'     `--' `--`--'.-'  /   `----'`--'
+ *                                                                     `---'
+ * Planet Player 播放器项目实践
+ * 音视频开发进阶知识星球专栏文章
+ *
+ * 作者：星陨
+ * 微信：ezglumes
+ * 微信公众号：音视频开发进阶
+ * 知识星球：音视频开发进阶
+ * 博客网站：https://glumes.com
+ *
+ * 欢迎联系交流！！！
+ */
+#include "gtest/gtest.h"
+#include "utils/PlanetEnvironment.h"
+
+int main(int argc, char** argv) {
+    testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
+    listeners.Append(new testing::EmptyTestEventListener());
+
+    AddGlobalTestEnvironment(new planet::PlanetEnvironment());
+    testing::InitGoogleTest(&argc,argv);
+
+    return RUN_ALL_TESTS();
+}
